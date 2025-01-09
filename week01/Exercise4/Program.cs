958aa73977aa01b488f3183423269fb6c27953a3
Poryoculus <rodrigopachecojor@gmail.com>
2025-01-09 15:03:46 -0300

    4th exercise

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        //declare list
+        List<int> numbers = new List<int>();
+        int sum = 0;
+        int number = -1;
+        int largest = int.MinValue;
+        
+
+        Console.WriteLine("Enter the list of numbers, type 0 when is it finished");
+        // while loop to start the sums
+        while (number != 0)
+        {
+            
+            number = int.Parse(Console.ReadLine());
+
+            if (number == 0) break;
+            sum += number ;
+            numbers.Add(number);
+            
+            
+        }
+        foreach (int numero in numbers)
+        {
+            if (numero > largest)
+            {
+                largest = numero;
+            }
+        }
+
+        float average = ((float)sum)  / numbers.Count;
+
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest numbers is: {largest}");
+        
+        
     }
 }

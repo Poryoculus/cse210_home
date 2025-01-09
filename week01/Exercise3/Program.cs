1f80d0c71cd660ceb75330f644b1cc83ef4502a3
Poryoculus <rodrigopachecojor@gmail.com>
2025-01-09 14:15:50 -0300

    guess the number

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,35 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string response = "yes";
+        do 
+        {
+            
+            Console.Write("What is the magic number?: ");
+
+            int magicnumber = int.Parse(Console.ReadLine());
+            int guess = 0;
+            int attempts = 0;
+            while (guess != magicnumber)
+            {
+                Console.Write("What is you magic number?: ");
+                guess = int.Parse(Console.ReadLine());
+                attempts += 1;
+                if (guess < magicnumber)
+                {
+                    Console.WriteLine("Higher");
+
+                }
+                else if (guess > magicnumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                
+            }
+            Console.WriteLine($"Congratulations! You guessed with {attempts} attempts");
+            Console.WriteLine("Do you want to continue? (yes/no)");
+            response = Console.ReadLine();
+
+        } while ( response == "yes");
     }
 }

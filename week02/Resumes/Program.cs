d76bb41a871b50da292e643738b351756ee891b0
Poryoculus <rodrigopachecojor@gmail.com>
2025-01-17 20:19:02 -0300

    resume project

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._company = "Sony";
+        job1._jobTitle = "Data Analyst";
+        job1._startYear = 2022;
+        job1._endYear = 2024;
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "Data Scientist";
+        job2._startYear = 2010;
+        job2._endYear = 2020; 
+
+        Resume resume1 = new Resume();
+        resume1._name = "Juan Albercado";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+
+        resume1.Display();
     }
 }

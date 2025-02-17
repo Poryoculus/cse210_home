9d2c06cbb341c7dbaa5d3c76d5cbdf5a3403e6f2
Poryoculus <rodrigopachecojor@gmail.com>
2025-02-17 01:50:23 -0300

    Week06 Shape assignments

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square square = new Square("red" , 4);
+        Rectangle myRectangle = new Rectangle("Red", 5.0, 3.0);
+        Circle myCircle = new Circle("Blue", 4.5);
+
+        
+
+        List<Shape> myList = new List<Shape>();
+
+        myList.Add(square);
+        myList.Add(myCircle);
+        myList.Add(myRectangle);
+
+        foreach (Shape form in myList)
+        {
+            Console.WriteLine($"Your Area: {form.GetArea()} and it's color is {form.GetColor()}");
+        }
     }
 }
